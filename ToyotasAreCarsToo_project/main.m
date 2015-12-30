9c0b1feddca1f20c081560a258811ea032153b8d
KrazeeKoder <tony.tulai@gmail.com>
2015-12-29 19:11:43 -0500

    Initial Commit

---
+++ ToyotasAreCarsToo_project/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ToyotasAreCarsToo_project
+//
+//  Created by Anthony Tulai on 2015-12-29.
+//  Copyright © 2015 Anthony Tulai. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
